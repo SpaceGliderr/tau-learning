24a07270de286d15ecc0bf9f414b12bef2f47ad0
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-05-17 14:20:57 +0800

    Add handling JSON response types

--- python-api-testing-course/tests/people_test.py
+++ python-api-testing-course/tests/people_test.py
@@ -1,10 +1,14 @@
 import json
+from pathlib import Path
+import random
 import uuid
 from assertpy.assertpy import assert_that, soft_assertions
+import pytest
 
 import requests
 
 from utils.config import BASE_URI
+import jsonpath_ng
 
 
 def create_new_unique_user():
@@ -120,7 +124,55 @@ def test_read_all_has_kent_with_soft_assertion():
     response = requests.get(BASE_URI) # Good idea to isolate config from tests
 
     with soft_assertions():
-        assert_that(response.status_code).is_equal_to(204)
+        assert_that(response.status_code).is_equal_to(200)
         # Use the json method to convert JSON string into dict
         response_text = response.json()
-        assert_that(response_text).extracting("fname").contains("zsakljdbfsda").is_not_empty()
+        assert_that(response_text).extracting("fname").contains("Kent").is_not_empty()
+
+
+@pytest.fixture
+def create_data():
+    # Reminder that fixtures are used for test setup and teardown
+
+    # Path.cwd() is to get the current working directory
+    # Then join paths with the provided strings and returns a Path object
+    # Using pathlib ensures that all paths created are cross-platform and can work easily
+    # Instead of using `with open(file)`, can use `path.open(mode="")` to ensure that `open` understands the file path
+    # with open(str(Path.cwd().joinpath('tests', 'data').joinpath("create_person.json")), "r") as f:
+    with open("tests/data/create_person.json", "r") as f:
+        payload = json.load(f)
+
+    random_num = random.randint(0, 1000)
+    last_name = f"Testing {random_num}"
+
+    payload["lname"] = last_name
+    yield payload
+
+
+def create_person_with_unique_last_name(body):
+    unique_last_name = body["lname"]
+    payload = json.dumps(body)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+
+    response = requests.post(url=BASE_URI, data=payload, headers=headers)
+
+    return response, unique_last_name
+
+
+def test_person_can_be_added_with_a_json_template(create_data):
+    create_person_with_unique_last_name(create_data)
+
+    response = requests.get(BASE_URI)
+    peoples = json.loads(response.text)
+
+    # Gets all last names for any object in the root array
+    # From my understanding it works similar to lxml etree.XPath
+    jsonpath_expr = jsonpath_ng.parse("$.[*].lname")
+    result = [match.value for match in jsonpath_expr.find(peoples)]
+
+    expected_last_name = create_data["lname"]
+    assert_that(result).contains(expected_last_name)
